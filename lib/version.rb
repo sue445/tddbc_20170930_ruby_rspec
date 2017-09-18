bff150c7a6e6070d32fcb945874de0faff03d023
sue445 <sue445@sue445.net>
2017-09-18 21:14:02 +0900

    Impl #==

--- lib/version.rb
+++ lib/version.rb
@@ -1,4 +1,6 @@
 class Version
+  attr_accessor :major, :minor, :patch
+
   def initialize(majar, minor, patch)
     @major = majar
     @minor = minor
@@ -7,5 +9,9 @@ class Version
 
   def to_s
     [@major, @minor, @patch].join(".")
+  end
+
+  def ==(other)
+    major == other.major && minor == other.minor && patch == other.patch
   end
 end
