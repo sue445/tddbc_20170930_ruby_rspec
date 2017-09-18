2a949491e25330ed60a9f3471c2425d355fec36c
sue445 <sue445@sue445.net>
2017-09-18 21:00:17 +0900

    Impl

--- lib/version.rb
+++ lib/version.rb
@@ -1,2 +1,11 @@
 class Version
+  def initialize(majar, minor, patch)
+    @major = majar
+    @minor = minor
+    @patch = patch
+  end
+
+  def to_s
+    [@major, @minor, @patch].join(".")
+  end
 end
