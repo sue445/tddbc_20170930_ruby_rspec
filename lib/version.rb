b09e38184b6c9ebc82131696861c5041a9ca6355
sue445 <sue445@sue445.net>
2017-09-18 22:10:06 +0900

    Refactor

--- lib/version.rb
+++ lib/version.rb
@@ -2,42 +2,9 @@ class Version
   attr_accessor :major, :minor, :patch
 
   def initialize(major, minor, patch)
-    raise ArgumentError, "majorがnilです" if major.nil?
-    raise ArgumentError, "majorが空文字です" if major == ""
-
-    if major.is_a?(String)
-      raise ArgumentError, "majorが数字以外です" unless major.match?(/^[0-9]+$/)
-      major = major.to_i
-    end
-
-    raise ArgumentError, "majorが負数です" if major < 0
-
-
-    raise ArgumentError, "minorがnilです" if minor.nil?
-    raise ArgumentError, "minorが空文字です" if minor == ""
-
-    if minor.is_a?(String)
-      raise ArgumentError, "minorが数字以外です" unless minor.match?(/^[0-9]+$/)
-      minor = minor.to_i
-    end
-
-    raise ArgumentError, "minorが負数です" if minor < 0
-
-
-    raise ArgumentError, "patchがnilです" if patch.nil?
-    raise ArgumentError, "patchが空文字です" if patch == ""
-
-    if patch.is_a?(String)
-      raise ArgumentError, "patchが数字以外です" unless patch.match?(/^[0-9]+$/)
-      patch = patch.to_i
-    end
-
-    raise ArgumentError, "patchが負数です" if patch < 0
-
-
-    @major = major
-    @minor = minor
-    @patch = patch
+    @major = check_version_number("major", major)
+    @minor = check_version_number("minor", minor)
+    @patch = check_version_number("patch", patch)
   end
 
   def to_s
@@ -46,5 +13,21 @@ class Version
 
   def ==(other)
     major == other.major && minor == other.minor && patch == other.patch
+  end
+
+  private
+
+  def check_version_number(name, variable)
+    raise ArgumentError, "#{name}がnilです" if variable.nil?
+    raise ArgumentError, "#{name}が空文字です" if variable == ""
+
+    if variable.is_a?(String)
+      raise ArgumentError, "#{name}が数字以外です" unless variable.match?(/^[0-9]+$/)
+      variable = variable.to_i
+    end
+
+    raise ArgumentError, "#{name}が負数です" if variable < 0
+
+    variable
   end
 end
