bf194349a694a639da14bc9d451b1432e66f5579
sue445 <sue445@sue445.net>
2017-09-18 20:44:32 +0900

    Add failed spec

--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -1,2 +1,11 @@
 describe Version do
+  describe "#to_s" do
+    subject { version.to_s }
+
+    context "major, minor, patch にそれぞれ 1, 4, 2 を与えた時に" do
+      let(:version) { Version.new(1, 4, 2) }
+
+      it { should eq "1.4.2" }
+    end
+  end
 end
