c385043698735c148e2be6b974d21be6d9897d4b
sue445 <sue445@sue445.net>
2017-09-18 21:11:51 +0900

    Add failed spec

--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -8,4 +8,20 @@ describe Version do
       it { should eq "1.4.2" }
     end
   end
+
+  describe "#==" do
+    it "バージョン 1.4.2 は バージョン 2.1.0 と等しくないこと" do
+      version1 = Version.new(1, 4, 2)
+      version2 = Version.new(2, 1, 0)
+
+      is_asserted_by { version1 != version2 }
+    end
+
+    it "バージョン 1.4.2 は バージョン 1.4.2 と等しいこと" do
+      version1 = Version.new(1, 4, 2)
+      version2 = Version.new(1, 4, 2)
+
+      is_asserted_by { version1 == version2 }
+    end
+  end
 end
